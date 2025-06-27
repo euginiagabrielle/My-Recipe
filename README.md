95e737ff1bcefc61453f34b620abee305b26accf
euginiagabrielle <icin2004@gmail.com>
2025-06-28 01:48:04 +0700

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
-# uas_c14220050
+# Simple Recipe Keeper - My Recipes
 
-A new Flutter project.
+Fitur:
+- Get started screen: muncul saat user pertama kali membuka aplikasi.
+- Login & register: autentikasi user menggunakan email dan password.
+- Lihat daftar resep: menampilkan semua daftar resep yang dimiliki user.
+- Tambah resep baru: menambahkan resep ke daftar resep dengan memasukkan nama, deskripsi, serta bahan masak.
+- Real-time update: data resep dapat muncul langsung di list resep setelah ditambahkan.
+- Hive storage: menyimpan data user di localstorage agar user tidak perlu login ulang saat membuka aplikasi.
+
+Langkah install dan build:
+1. Clone repositori 
+git clone https://github.com/euginiagabrielle/My-Recipe
+2. Masuk ke folder proyek
+cd My-Recipe
+3. Install dependencies:
+flutter pub get
+4. Run aplikasi
+flutter run
+
+Teknologi yang digunakan:
+- Flutter
+- Dart
+- Supabase
+- Hive
+
+Dummy user login:
+- Email: ambw@gmail.com
+- Password: ambw123
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:uas_c14220050/auth/auth_gate.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -8,10 +7,9 @@ import 'package:uas_c14220050/pages/get_started_page.dart';
 void main() async {
   await Hive.initFlutter();
   final box = await Hive.openBox('myAuth');
-  await dotenv.load(fileName: ".env");
   await Supabase.initialize(
-    anonKey: dotenv.env['SUPABASE_ANONKEY']!,
-    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2b3hhbHN2aXhnaHJuc3ZlaXFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4MzI4NjAsImV4cCI6MjA2NjQwODg2MH0.yb9X8a1k5wcyoSbKVE8uOWw23QtFtdrkHjEuR1IgB28",
+    url: "https://avoxalsvixghrnsveiqo.supabase.co",
   );
   final isFirstTime = box.get('isFirstTime', defaultValue: true);
   runApp(MainApp(isFirstTime: isFirstTime));
